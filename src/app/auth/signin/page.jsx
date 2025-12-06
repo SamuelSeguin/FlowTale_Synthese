98dea7fe4683bb902d87402de2b00d7e13ce6935
Emilie Paquin <2073129@cstj.qc.ca>
2025-12-06 11:26:27 -0500

    modification tab title auth + icon

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -1,6 +1,10 @@
 import SignInClient from "@/app/_components/SignInClient";
 import NavBar from "../../_components/NavBar";
 
+export const metadata = {
+  title: "Se connecter",
+};
+
 const SignInPage = () => {
   return (
     <div>
